12d778417b510fe35514d303f8fd51d5553fe59e
ETCasual <casualorient@gmail.com>
2022-02-10 18:16:16 +0800

    Updated Tagline

--- src/routes/gamescreen.tsx
+++ src/routes/gamescreen.tsx
@@ -4,7 +4,7 @@ import { Container } from '../components/Container';
 import { motion } from 'framer-motion';
 import { answers } from '../config/constants';
 
-import { SimpleGrid, Text, Center, CenterProps, keyframes } from '@chakra-ui/react';
+import { SimpleGrid, Text, Center, CenterProps, keyframes, Flex } from '@chakra-ui/react';
 
 import { useDisclosure } from '@chakra-ui/react';
 
@@ -23,9 +23,14 @@ export const GameScreen = () => {
 
   return status === 'success' ? (
     <Container height="100vh">
-      <Text fontSize="6vw" fontFamily={'zcool'}>
-        择偶条件
-      </Text>
+      <Flex>
+        <Text fontSize="6vw" fontFamily={'zcool'}>
+          美貌与智慧
+        </Text>
+        <Text fontSize="6vw" fontFamily={'zcool'} color="#FF69B4">
+          你会怎样选?
+        </Text>
+      </Flex>
       <SimpleGrid columns={2} spacing={10}>
         {Object.keys(data).map((d) =>
           d !== 'NO_ID_FIELD' ? <GridBox answers={answers} data={data} d={d} /> : null
