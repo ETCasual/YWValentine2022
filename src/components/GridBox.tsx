339a9f6fca206369e26e784001041f87b2d858a7
ETCasual <casualorient@gmail.com>
2022-02-11 14:57:54 +0800

    Rearranging 2

--- src/components/GridBox.tsx
+++ src/components/GridBox.tsx
@@ -2,7 +2,7 @@ import { Box, Center, ScaleFade } from '@chakra-ui/react';
 
 export const GridBox = ({ data, answers, d }) => {
   return (
-    <Center key={d} w="1000px" h="135px" bg="pink" rounded={'2xl'} shadow="xl">
+    <Center key={d} w="1000px" h="115px" bg="pink" rounded={'2xl'} shadow="xl">
       <Center fontSize="55px" fontFamily={'noto'}>
         {!data[d] ? (
           <Box
